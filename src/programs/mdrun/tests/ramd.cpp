015bdd577d822df57d10eaba10a929c9c1e8be3e
Bernd Doser <bernd.doser@h-its.org>
2019-07-25 13:58:49 +0200

    remove duplicated integration
    of commit aafab0 and add input files of 1wdhi and membrane

--- src/programs/mdrun/tests/ramd.cpp
+++ src/programs/mdrun/tests/ramd.cpp
@@ -32,9 +32,9 @@ RAMDTestFixture::~RAMDTestFixture()
 //! Test fixture for mdrun with RAMD settings
 typedef gmx::test::RAMDTestFixture RAMDTest;
 
-TEST_F(RAMDTest, RAMDCanRun)
+TEST_F(RAMDTest, RAMD_1whhi)
 {
-    runner_.useTopGroAndNdxFromDatabase("spc2");
+    runner_.useTopGroAndNdxFromDatabase("1wdhi");
     const std::string mdpContents = R"(
         dt                       = 0.004
         nsteps                   = 2
@@ -44,17 +44,48 @@ TEST_F(RAMDTest, RAMDCanRun)
         ref-t                    = 300
         constraints              = all-bonds
         cutoff-scheme            = Verlet
+        ramd                     = yes
+        ramd-seed                = 9876
+        ramd-force               = 600.0
+        ramd-eval-freq           = 50
+        ramd-r-min-dist          = 0.025
+        ramd-force-out-freq      = 100
+        ramd-max-dist            = 4.0
+        ramd-receptor            = Protein
+        ramd-ligand              = INH
     )";
-//        ramd                     = yes
-//        ramd-seed                = 9876
-//        ramd-force               = 600.0
-//        ramd-eval-freq           = 50
-//        ramd-r-min-dist          = 0.025
-//        ramd-force-out-freq      = 100
-//        ramd-max-dist            = 4.0
-//        ramd-receptor            = FirstWaterMolecule
-//        ramd-ligand              = SecondWaterMolecule
-//    )";
+    runner_.useStringAsMdpFile(mdpContents);
+
+    EXPECT_EQ(0, runner_.callGrompp());
+
+    ::gmx::test::CommandLine caller;
+
+    // Do an mdrun with RAMD enabled
+    ASSERT_EQ(0, runner_.callMdrun(caller));
+}
+
+TEST_F(RAMDTest, RAMD_membrane)
+{
+    runner_.useTopGroAndNdxFromDatabase("membrane");
+    const std::string mdpContents = R"(
+        dt                       = 0.004
+        nsteps                   = 2
+        tcoupl                   = Berendsen
+        tc-grps                  = System
+        tau-t                    = 0.5
+        ref-t                    = 300
+        constraints              = all-bonds
+        cutoff-scheme            = Verlet
+        ramd                     = yes
+        ramd-seed                = 9876
+        ramd-force               = 600.0
+        ramd-eval-freq           = 50
+        ramd-r-min-dist          = 0.025
+        ramd-force-out-freq      = 100
+        ramd-max-dist            = 4.0
+        ramd-receptor            = Protein
+        ramd-ligand              = IXO
+    )";
     runner_.useStringAsMdpFile(mdpContents);
 
     EXPECT_EQ(0, runner_.callGrompp());
