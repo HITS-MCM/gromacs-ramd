a89b1a0dd63632a9c034636452b97a3515e7538c
Bernd Doser <bernd.doser@h-its.org>
2020-11-03 10:21:39 +0100

    new parameters for multi-ligand RAMD

--- src/gromacs/mdtypes/ramd_params.h
+++ src/gromacs/mdtypes/ramd_params.h
@@ -40,32 +40,32 @@
 #include "gromacs/utility/basedefinitions.h"
 #include "gromacs/utility/real.h"
 
+#include <vector>
+
 namespace gmx
 {
 
 /// Parameters for RAMD group
 struct RAMDGroup
 {
-    int  nat; ///< Number of atoms in the RAMD group
-    int* ind; ///< The global atoms numbers
+    std::vector<int> receptor; ///< The global atoms numbers of the receptor
+    std::vector<int> ligand;   ///< The global atoms numbers of the ligand
+    real force;                ///< Force to be applied in kcal/mol/Angstrom
+    real max_dist;             ///< Specifies the distance in Angstrom between the COMs of the ligand
+                               ///  and the receptor when the simulation is stopped
+    real r_min_dist;           ///< Specifies the minimum distance in Angstrom
+                               ///  to be traveled by the ligand in one RAMD step
 };
 
 /// Parameters for Random Acceleration Molecular Dynamics (RAMD)
 struct RAMDParams
 {
-    int64_t seed;        ///< Initialization number for pseudo random number generator
-    real    force;       ///< Force to be applied in kcal/mol/Angstrom
-    int     eval_freq;   ///< Number of MD steps in one RAMD step
-    real    r_min_dist;  ///< Specifies the minimum distance in Angstrom
-                         ///  to be traveled by the ligand in one RAMD step
-    int  force_out_freq; ///< Every 'force_out_freq' steps detailed output of forces will be written
-    real max_dist;       ///< Specifies the distance in Angstrom between the COMs of the ligand
-                         ///  and the protein when the simulation is stopped
-    RAMDGroup protein;   ///< The name of the protein group, is looked up in the index file or
-                         ///  in the default groups to obtain the atoms involved
-    RAMDGroup ligand;    ///< The name of the protein group, is looked up in the index file or
-                         ///  in the default groups to obtain the atoms involved
-    gmx_bool old_angle_dist; ///< Use old angle distribution
+    int64_t seed;                  ///< Initialization number for pseudo random number generator
+    int ngroup;                    ///< Number of RAMD groups
+    std::vector<RAMDGroup> group;  ///< List of RAMD receptor-ligand pairs
+    int eval_freq;                 ///< Number of MD steps in one RAMD step
+    int force_out_freq;            ///< Every 'force_out_freq' steps detailed output of forces will be written
+    gmx_bool old_angle_dist;       ///< Use old angle distribution
 };
 
 } // namespace gmx
