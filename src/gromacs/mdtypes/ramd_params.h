3797eb146b0bddf5d7662b0c43b169367275ba73
Bernd Doser <bernd.doser@h-its.org>
2024-02-09 13:26:45 +0000

    Add binding residues to RAMD group
    Refactor RAMD group initialization to use vector resizing

--- src/gromacs/mdtypes/ramd_params.h
+++ src/gromacs/mdtypes/ramd_params.h
@@ -33,6 +33,8 @@
  */
 #pragma once
 
+#include <vector>
+
 #include "gromacs/utility/basedefinitions.h"
 #include "gromacs/utility/real.h"
 
@@ -42,23 +44,24 @@ namespace gmx
 /// Parameters for RAMD group
 struct RAMDGroup
 {
-    real force;      ///< Force to be applied in kcal/mol/Angstrom
-    real max_dist;   ///< Specifies the distance in Angstrom between the COMs of the ligand
-                     ///  and the receptor when the simulation is stopped
-    real r_min_dist; ///< Specifies the minimum distance in Angstrom
-                     ///  to be traveled by the ligand in one RAMD step
+    real force;                        ///< Force to be applied in kcal/mol/Angstrom
+    real max_dist;                     ///< Specifies the distance in Angstrom between the COMs of the ligand
+                                       ///  and the receptor when the simulation is stopped
+    real r_min_dist;                   ///< Specifies the minimum distance in Angstrom
+                                       ///  to be traveled by the ligand in one RAMD step
+    std::vector<int> binding_residues; ///< The global atoms numbers of the binding residues
 };
 
 /// Parameters for Random Acceleration Molecular Dynamics (RAMD)
 struct RAMDParams
 {
-    int64_t seed;               ///< Initialization number for pseudo random number generator
-    int ngroup;                 ///< Number of RAMD groups
-    RAMDGroup* group;           ///< List of RAMD receptor-ligand pairs
-    int eval_freq;              ///< Number of MD steps in one RAMD step
-    int force_out_freq;         ///< Every 'force_out_freq' steps detailed output of forces will be written
-    gmx_bool old_angle_dist;    ///< Use old angle distribution
-    gmx_bool connected_ligands; ///< Behavior of re-entering ligands into the dissociation radius
+    int64_t seed;                 ///< Initialization number for pseudo random number generator
+    int ngroup;                   ///< Number of RAMD groups
+    std::vector<RAMDGroup> group; ///< List of RAMD receptor-ligand pairs
+    int eval_freq;                ///< Number of MD steps in one RAMD step
+    int force_out_freq;           ///< Every 'force_out_freq' steps detailed output of forces will be written
+    gmx_bool old_angle_dist;      ///< Use old angle distribution
+    gmx_bool connected_ligands;   ///< Behavior of re-entering ligands into the dissociation radius
 };
 
 } // namespace gmx
